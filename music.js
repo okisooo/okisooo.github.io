061df607fec129602e2b7d7bf3c8dbecf01745a2
Samuele Santonicola <samuelesantonicola@gmail.com>
2023-06-29 00:58:37 +0200

    Update music.js

--- music.js
+++ music.js
@@ -1,27 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var audio = document.getElementById("myAudio");
-  var button = document.getElementById("playButton");
-  var volumeBar = document.getElementById("volumeBar");
+var audio = document.getElementById("myAudio");
+var button = document.getElementById("playButton");
+var volumeBar = document.getElementById("volumeBar");
 
-  function toggleAudio() {
-    if (audio.paused) {
-      audio.play();
-      button.classList.add("playing");
-    } else {
-      audio.pause();
-      button.classList.remove("playing");
-    }
+function toggleAudio() {
+  if (audio.paused) {
+    audio.play();
+    button.classList.add("playing");
+  } else {
+    audio.pause();
+    button.classList.remove("playing");
   }
+}
 
-  button.addEventListener("click", toggleAudio);
+button.addEventListener("click", toggleAudio);
 
-  volumeBar.addEventListener("input", function() {
-    audio.volume = volumeBar.value;
-  });
-
-  // Set initial volume from the volume bar value
+volumeBar.addEventListener("input", function() {
   audio.volume = volumeBar.value;
-
-  // Remove autoplay attempt for compatibility
-  button.style.display = "block";
 });
+
+// Set initial volume from the volume bar value
+audio.volume = volumeBar.value;
+
+// Remove autoplay attempt for compatibility
+button.style.display = "block";
