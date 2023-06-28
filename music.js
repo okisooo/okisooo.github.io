e48bf8a9ff35099d773c0237b71f344b63e27c51
Samuele Santonicola <samuelesantonicola@gmail.com>
2023-06-28 16:40:30 +0200

    updatre

--- music.js
+++ music.js
@@ -1,9 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
   var audio = document.getElementById("myAudio");
   var button = document.getElementById("playButton");
+  var volumeBar = document.getElementById("volumeBar");
 
   button.addEventListener("click", function() {
-    audio.volume = 1; // Set volume to maximum (1)
-    audio.play();
+    if (audio.paused) {
+      audio.play();
+      button.textContent = "Pause";
+    } else {
+      audio.pause();
+      button.textContent = "Play";
+    }
   });
+
+  volumeBar.addEventListener("input", function() {
+    audio.volume = volumeBar.value;
+  });
+
+  // Auto-play on startup
+  audio.play();
 });
