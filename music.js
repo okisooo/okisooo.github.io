02cad5577a5a41ace4e6f3ae6f4455ce3b17084b
Samuele Santonicola <samuelesantonicola@gmail.com>
2023-06-28 16:02:46 +0200

    update

--- music.js
+++ music.js
@@ -1,20 +1,7 @@
 var audio = document.getElementById("myAudio");
+var button = document.getElementById("playButton");
 
-function playAudio() {
-  audio.volume = 0; // Start with volume set to 0
+button.addEventListener("click", function() {
+  audio.volume = 1; // Set volume to maximum (1)
   audio.play();
-
-  var fadeInterval = setInterval(function() {
-    // Gradually increase volume by 0.01 every 100 milliseconds
-    audio.volume += 0.01;
-
-    if (audio.volume >= 1) {
-      // Volume reached 1 (maximum), clear the interval
-      clearInterval(fadeInterval);
-    }
-  }, 100);
-}
-
-function pauseAudio() {
-  audio.pause();
-}
+});
