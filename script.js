e7e94ee86c771bd245955f4eb4527fa9316e6cf3
Samuele Santonicola <samuelesantonicola@gmail.com>
2023-09-10 01:04:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//yo
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the video and unmute button
+  const video = document.getElementById("background-video");
+  const unmuteButton = document.getElementById("unmute-button");
+
+  // Add a click event listener to the unmute button
+  unmuteButton.addEventListener("click", function () {
+      if (video.muted) {
+          video.muted = false;
+          unmuteButton.textContent = "Mute"; // Change the button text to "Mute" when unmuted
+      } else {
+          video.muted = true;
+          unmuteButton.textContent = "Unmute"; // Change the button text to "Unmute" when muted
+      }
+  });
+});
