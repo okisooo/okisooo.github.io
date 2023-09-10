c4f575c5152a2bf661e4ebc665058ae6a3d42e75
Samuele Santonicola <samuelesantonicola@gmail.com>
2023-09-10 04:00:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Get the unmute button
     const unmuteButton = document.getElementById('unmute-button');
 
-    let audioContext; // Declare audioContext variable
-    let gainNode; // Declare gainNode variable for volume control
-
     // Flag to track mute/unmute state
     let isMuted = true;
 
@@ -17,27 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // Mute immediately
             video.muted = true;
         } else {
-            // Unmute with fade effect
-            if (!audioContext) {
-                // Create the audio context (only on the first unmute)
-                audioContext = new (window.AudioContext || window.webkitAudioContext)();
-                gainNode = audioContext.createGain(); // Create gain node for volume control
-                gainNode.connect(audioContext.destination); // Connect gain node to audio context
-            }
-
-            // Gradually increase the gain (volume) over 0.5 seconds
-            const fadeDuration = 0.5; // seconds
-
-            gainNode.gain.setValueAtTime(0, audioContext.currentTime); // Start with the gain at 0
-
-            gainNode.gain.linearRampToValueAtTime(1, audioContext.currentTime + fadeDuration); // Linearly ramp the gain to 1 (full volume)
-
-            // Connect the video's audio to the gain node
-            const videoAudio = video.captureStream().getAudioTracks()[0];
-            const audioSource = audioContext.createMediaStreamSource(new MediaStream([videoAudio]));
-            audioSource.connect(gainNode);
-
-            // Unmute the video
+            // Unmute immediately
             video.muted = false;
         }
 
