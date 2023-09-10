c3c799f12c35ecf39dabf5ab8d901237a97a68fe
Samuele Santonicola <samuelesantonicola@gmail.com>
2023-09-10 03:34:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the video element
+    // Get the video element and audio context
     const video = document.getElementById('background-video');
+    let audioContext; // Declare audioContext variable
 
     // Get the unmute button
     const unmuteButton = document.getElementById('unmute-button');
@@ -8,34 +9,41 @@ document.addEventListener('DOMContentLoaded', function () {
     // Flag to track mute/unmute state
     let isMuted = true;
 
-    // Add a click event listener to the unmute button
+    // Add a click event listener to the mute/unmute button
     unmuteButton.addEventListener('click', () => {
         if (isMuted) {
             // Mute immediately
             video.muted = true;
         } else {
             // Unmute with fade effect
-            const audioElement = video.querySelector('audio');
-            if (audioElement) {
-                // Create an AudioContext only if there's an audio element
-                const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-                // Create audio source and gain nodes
-                const audioSource = audioContext.createMediaElementSource(audioElement);
-                const gainNode = audioContext.createGain();
-
-                // Connect the audio source to the gain node
-                audioSource.connect(gainNode);
-                gainNode.connect(audioContext.destination);
-
-                // Gradually increase the gain (volume) over 0.5 seconds
-                const fadeDuration = 0.5; // seconds
-                const finalVolume = 1; // Full volume
-                const currentTime = audioContext.currentTime;
-
-                gainNode.gain.setValueAtTime(0, currentTime);
-                gainNode.gain.linearRampToValueAtTime(finalVolume, currentTime + fadeDuration);
+            if (!audioContext) {
+                // Create the audio context (only on the first unmute)
+                audioContext = new (window.AudioContext || window.webkitAudioContext)();
             }
+
+            const audioElement = document.createElement('audio');
+            audioElement.src = video.src;
+
+            const audioSource = audioContext.createMediaElementSource(audioElement);
+            const gainNode = audioContext.createGain();
+
+            // Connect the audio source to the gain node
+            audioSource.connect(gainNode);
+            gainNode.connect(audioContext.destination);
+
+            // Gradually increase the gain (volume) over 0.5 seconds
+            const fadeDuration = 0.5; // seconds
+            const finalVolume = 1; // Full volume
+            const currentTime = audioContext.currentTime;
+
+            gainNode.gain.setValueAtTime(0, currentTime);
+            gainNode.gain.linearRampToValueAtTime(finalVolume, currentTime + fadeDuration);
+
+            // Start audio context (required by browsers)
+            audioContext.resume().then(() => {
+                // Play audio element
+                audioElement.play();
+            });
 
             // Unmute the video
             video.muted = false;
