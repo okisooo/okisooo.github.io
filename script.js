54200b4ebfa02553f783672dccabcb68edc7e3bb
Samuele Santonicola <samuelesantonicola@gmail.com>
2023-09-15 20:58:17 +0200

    Audio updates

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ $(document).ready(function() {
   // Flag to track mute/unmute state
   let isMuted = true;
 
+  // Flag to track whether the video has been unmuted on first hover
+  let isFirstHover = true;
+
   // Function to update the button text
   function updateButtonText() {
     if (isMuted) {
@@ -29,53 +32,62 @@ $(document).ready(function() {
     fadingText.css('transform', 'translate(-50%, -50%) scale(1)');
     fadingText.css('transform-origin', '50% 50%');
 
-    // Start the video and then start the animation after a 2-second delay
-    video.addEventListener('play', function() {
-      setTimeout(function() {
-        // Start the animation
+    // Start the animation after a 2-second delay
+    setTimeout(function() {
+      // Start the animation
+      fadingText.animate({
+        opacity: 1,
+        fontSize: '64px',
+        transform: 'translate(-50%, -50%) scale(1.3)'
+      }, 2500, 'easeInOutCubic', function() {
+        // Animation complete
         fadingText.animate({
-          opacity: 1,
-          fontSize: '64px',
-          transform: 'translate(-50%, -50%) scale(1.3)'
-        }, 2500, 'easeInOutCubic', function() {
+          fontSize: '64px'
+        }, 8500, 'easeInOutCubic', function() {
           // Animation complete
           fadingText.animate({
-            fontSize: '64px'
-          }, 8500, 'easeInOutCubic', function() {
-            // Animation complete
-            fadingText.animate({
-              opacity: 0,
-              fontSize: '18px',
-              transform: 'translate(-50%, -50%) scale(1.2)'
-            }, 500, 'easeInOutCubic');
-          });
+            opacity: 0,
+            fontSize: '18px',
+            transform: 'translate(-50%, -50%) scale(1.2)'
+          }, 500, 'easeInOutCubic');
         });
-      }, 3500); // Delay the animation by 3500 milliseconds (3.5 seconds)
-    });
-
-    // Add a click event listener to the unmute button
-    unmuteButton.on('click', function() {
-      if (isMuted) {
-        // Unmute immediately
-        video.muted = false;
-      } else {
-        // Mute immediately
-        video.muted = true;
-      }
-
-      // Toggle mute state
-      isMuted = !isMuted;
-
-      // Update the button text
-      updateButtonText();
-    });
-
-    // Start the video
-
+      });
+    }, 2000); // Delay the animation by 2000 milliseconds (2 seconds)
   }
 
+  // Add a click event listener to the unmute button
+  unmuteButton.on('click', function() {
+    if (isMuted) {
+      // Unmute immediately
+      video.muted = false;
+    } else {
+      // Mute immediately
+      video.muted = true;
+    }
+
+    // Toggle mute state
+    isMuted = !isMuted;
+
+    // Update the button text
+    updateButtonText();
+  });
+
+  // Add an event listener for mouseenter to unmute the video on first hover
+  $(document).one('mouseenter', function() {
+    if (isFirstHover) {
+      if (isMuted) {
+        // Unmute immediately when the mouse enters the document for the first time
+        video.muted = false;
+        isMuted = false; // Update the mute state
+        updateButtonText(); // Update the button text after unmuting
+      }
+      isFirstHover = false; // Set isFirstHover to false to prevent further unmuting
+    }
+  });
+
+  // Start the video
   video.play();
 
-  // Show and animate the text after a delay
+  // Show and animate the text
   animateText();
 });
