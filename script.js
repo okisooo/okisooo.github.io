19bb21a7cc89c91f73d6d1b9e90f6a2b35c7c338
Samuele Santonicola <samuelesantonicola@gmail.com>
2023-10-19 20:52:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function() {
     const unmuteButton = $('#unmute-button');
     const video = document.getElementById('bgvid');
     let isMuted = video.muted;
+    let fadeInterval;
 
     function updateButtonText() {
         if (isMuted) {
@@ -11,18 +12,31 @@ window.onload = function() {
         }
     }
 
+    function fadeVolume(direction) {
+        clearInterval(fadeInterval);
+        fadeInterval = setInterval(function() {
+            if (direction === 'up' && video.volume < 1) {
+                video.volume += 0.1;
+            } else if (direction === 'down' && video.volume > 0.1) { // Ensure volume is within the range [0, 1]
+                video.volume -= 0.1;
+            } else {
+                clearInterval(fadeInterval);
+            }
+        }, 50);
+    }
+
     unmuteButton.on('click', function() {
         // Toggle mute state
         isMuted = !isMuted;
-
+    
         if (isMuted) {
-            // Mute immediately
-            video.muted = true;
+            // Fade out volume
+            fadeVolume('down');
         } else {
-            // Unmute immediately
-            video.muted = false;
+            // Fade in volume
+            fadeVolume('up');
         }
-
+    
         updateButtonText();
     });
 
