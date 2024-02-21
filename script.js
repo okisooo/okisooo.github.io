0ec5766dda29f96222d0429fc6fa32ae29997c38
oxo <samuelesantonicola@gmail.com>
2024-02-21 23:20:25 +0100

    Update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const time = document.getElementById("clock");
+const clock = document.getElementById("clock");
 
 function updateClock() {
   const now = new Date();
   const hours = now.getHours().toString().padStart(2, "0");
   const minutes = now.getMinutes().toString().padStart(2, "0");
   const seconds = now.getSeconds().toString().padStart(2, "0");
-  time.textContent = `${hours}:${minutes}:${seconds}`;
+  clock.textContent = `${hours}:${minutes}:${seconds}`;
 }
 
 updateClock();
