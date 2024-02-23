95cc2d172979b71f3cc22862bf02399ee82165b9
oxo <samuelesantonicola@gmail.com>
2024-02-23 21:11:07 +0100

    UPDATE DISCORD FETCH ICON

---
+++ script.js
@@ -0,0 +1,31 @@
+// Function to fetch Discord profile picture
+function fetchDiscordProfilePicture(userId) {
+    const apiUrl = `https://discordlookup.mesavirep.xyz/v1/user/${userId}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const profilePictureUrl = data.avatar.link;
+            // Update the Discord icon with the fetched profile picture
+            const discordIcon = document.getElementById("discord-icon");
+            discordIcon.style.backgroundImage = `url('${profilePictureUrl}')`;
+        })
+        .catch(error => console.error('Error fetching Discord profile picture:', error));
+}
+
+// Function to update clock
+function updateClock() {
+    const clock = document.getElementById("clock");
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    clock.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+// Call the function to fetch the Discord profile picture with the Discord user ID
+fetchDiscordProfilePicture("274178934143451137"); // Replace with the actual Discord user ID
+
+// Call the function to update the clock every second
+updateClock();
+setInterval(updateClock, 1000);
